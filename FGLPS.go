e860fcae39352be4b1e305c3cf116d9193b74bce
Clark McCauley <clark.mccauley@printtrackerpro.com>
2021-07-04 13:16:40 -0600

    Fixed wait group calls

--- FGLPS.go
+++ FGLPS.go
@@ -25,21 +25,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	wg.Add(1)
-
 	// Pushing events into the channel
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		for i := *argPortStart; i <= *argPortEnd; i++ {
 			targetHost := fmt.Sprintf("%s:%d", *argHost, i)
-			wg.Add(1)
 			portsChan <- targetHost
 		}
-		wg.Done()
 		close(portsChan)
 	}()
 
 	// Consuming events
 	for i := 0; i <= *argThreadsNum; i++ {
+		wg.Add(1)
 		go portScanner(portsChan, resChan, &wg)
 	}
 
@@ -56,9 +55,8 @@ func main() {
 
 // Scan a single port
 func portScanner(portsChan, resChan chan string, wg *sync.WaitGroup) {
-
+	defer wg.Done()
 	for targetHost := range portsChan {
-		defer wg.Done()
 		_, err := net.DialTimeout("tcp", targetHost, time.Millisecond*300)
 		if err == nil {
 			resChan <- targetHost
