59bdf39e327e77b998fe6ecf1552525a8f2a4e98
Steve Briley <24689390+SteveBriley@users.noreply.github.com>
2021-07-05 22:40:52 -0400

    Clarity and performance improvements
    - Converted single letter flags into spelled out flag names.
    - Added a flag for portTimeout.
    - Added input validation for all of the Int based CLI flags.
    - Added ERROR output for all flags evaluated in a single pass.
    - Set the default port scanning timeout to 5 seconds, which helps when port scanning hosts on the other side of the world.
    - Set the default threads to 65535 to support 100% parallel execution for the fastest completion times possible. This was previously set to 4.

--- main.go
+++ main.go
@@ -13,23 +13,44 @@ func main() {
 	wg := sync.WaitGroup{}
 	portsChan := make(chan string)
 	resChan := make(chan string)
-	argHost := flag.String("h", "", "Host to scan")
-	argPortStart := flag.Int("s", 1, "Start port (Default 1)")
-	argPortEnd := flag.Int("e", 100, "End port (Default 100)")
-	argThreadsNum := flag.Int("t", 4, "Threads count (Default 4)")
 
+	argHost := flag.String("host", "", "Host to scan")
+	argFirstPort := flag.Int("firstPort", 1, "First port of port range to scan (1-65535)")
+	argLastPort := flag.Int("lastPort", 100, "Last port of port range to scan (1-65535)")
+	argThreadsNum := flag.Int("threads", 65535, "Thread count. (maximum simultaneous port scans)")
+	argPortTimeout := flag.Int("portTimeout", 5, "Port timeout in seconds.")
 	flag.Parse()
 
+	showUsageInfo := false
 	if *argHost == "" {
-		flag.Usage()
-		os.Exit(1)
+		showUsageInfo = true
+	}
+	if *argFirstPort < 1 || *argFirstPort > 65535 {
+		fmt.Println("ERROR: -firstPort must be >= 1 and <= 65535")
+		showUsageInfo = true
+	}
+	if *argLastPort < 1 || *argLastPort > 65535 {
+		fmt.Println("ERROR: -lastPort must be >= 1 and <= 65535")
+		showUsageInfo = true
+	}
+	if *argThreadsNum < 1 || *argThreadsNum > 65535 {
+		fmt.Println("ERROR: -threads must be >= 1 and <= 65535")
+		showUsageInfo = true
+	}
+	if *argPortTimeout < 1 || *argPortTimeout > 65535 {
+		fmt.Println("ERROR: -portTimeout must be >= 1 and <= 65535")
+		showUsageInfo = true
+	}
+
+	if showUsageInfo {
+		showUsage()
 	}
 
 	// Pushing events into the channel
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for i := *argPortStart; i <= *argPortEnd; i++ {
+		for i := *argFirstPort; i <= *argLastPort; i++ {
 			targetHost := fmt.Sprintf("%s:%d", *argHost, i)
 			portsChan <- targetHost
 		}
@@ -39,7 +60,7 @@ func main() {
 	// Consuming events
 	for i := 0; i <= *argThreadsNum; i++ {
 		wg.Add(1)
-		go portScanner(portsChan, resChan, &wg)
+		go portScanner(portsChan, resChan, &wg, *argPortTimeout)
 	}
 
 	// Returning the results
@@ -53,11 +74,19 @@ func main() {
 	close(resChan)
 }
 
+func showUsage() {
+	fmt.Println("")
+	fmt.Println("Purpose: Rapidly TCP scans a port range for open ports.")
+	fmt.Println("")
+	flag.Usage()
+	os.Exit(1)
+}
+
 // Scan a single port
-func portScanner(portsChan, resChan chan string, wg *sync.WaitGroup) {
+func portScanner(portsChan, resChan chan string, wg *sync.WaitGroup, portTimeout int) {
 	defer wg.Done()
 	for targetHost := range portsChan {
-		_, err := net.DialTimeout("tcp", targetHost, time.Millisecond*300)
+		_, err := net.DialTimeout("tcp", targetHost, time.Second*time.Duration(portTimeout))
 		if err == nil {
 			resChan <- targetHost
 		}
